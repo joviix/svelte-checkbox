75e57ab260e77f838b30671880883a9d611a42b5
José Visliquez <jsxd.17@gmail.com>
2020-08-03 11:40:33 -0400

    [FIX] pkg was missing from rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,8 @@
 import svelte from "rollup-plugin-svelte";
 import resolve from "@rollup/plugin-node-resolve";
 
+const pkg = require('./package.json');
+
 export default {
   input: "Checkbox.svelte",
   output: [
